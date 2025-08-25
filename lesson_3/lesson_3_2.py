9bd06d00659ae50079e4243fcc1904f9f8db97b4
Daniil Nikolaev mac <danilfg@yahoo.com>
2025-08-25 21:43:00 +0300

    add lesson_3

---
+++ lesson_3/lesson_3_2.py
@@ -0,0 +1,34 @@
+# print(4 > 2)
+# print(4 > 9)
+# a = 11
+# b = 10
+# result = a >= b
+# print(type(result)
+
+# print(5 <= 7 - 2)
+# a = "asd"
+# b = "asd"
+# print(a != b)
+
+# x = 20
+# print(x % 2 == 0)
+
+# x = 27
+# print(x % 3 == 0) # наше число кратно 3
+
+# a = 1.987
+# # print(a >= 1 and a <= 1.986)
+# print(1 <= a <= 2)
+# name = "Даниил"
+# print(("Д" in name) or ("ф" in name))
+
+# x = 5
+# print(not ((x % 2 == 0) or (x % 3 == 0)))
+a = 2131
+print(bool(1))
+print(bool(0))
+print(bool(-10))
+print(bool("dasdfa"))
+print(bool(""))
+print(bool(a))
+
