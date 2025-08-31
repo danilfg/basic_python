9dcee3140fde09e1e78e87aee74c10ab43d5fbf1
Daniil Nikolaev mac <danilfg@yahoo.com>
2025-08-31 18:22:50 +0300

    add homework 4

---
+++ lesson_4/lesson_4_2.py
@@ -0,0 +1,26 @@
+# \n
+
+text = """Раз строка
+два строка
+три строка"""
+
+text2 = "Раз строка два строка три строка"
+text3 = "Раз строка\nдва строка\nтри строка"
+# print(len(text2))
+# print(len(text3))
+# print(text2)
+
+# text4 = "Раз строка\t\t\t\t\tдва строка три строка"
+# print(text4)
+# \n
+# \t
+
+# text5 = 'Просто текст, \'второй\' текст'
+#
+# print(text5)
+
+# path = "C:\\Pogram files\\Pycharm\\pycharm.exe"
+# print(path)
+
+path = r"C:\Pogram files\Pycharm\pycharm.exe"
+print(path)
